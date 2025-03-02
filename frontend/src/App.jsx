6fb74d8f8bce2a8e8d0bbf5955c65d028f157ad3
webNerd69 <rroy64330@gmail.com>
2025-03-02 18:35:49 +0530

    solving build issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { ToastContainer } from 'react-toastify'
 import { Route, Routes } from "react-router-dom";
 import PostsPage from './pages/PostsPage';
 import CommentPage from './pages/CommentPage';
-import MyPosts from './pages/myPosts';
 import Login from './pages/login';
 import Leaderboard from './pages/Leaderboard';
+import MyPosts from './pages/MyPosts';
 const App = () => {
   return (
     <div>
